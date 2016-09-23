35aa52711559219bcdb56813482c7fe7aecbc241
Mason Embry <masonembry81@mac.com>
2016-09-23 10:25:50 -0700

    Prices are adding.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
+////////// Interactive Form //////////
+//
+// Author: Mason Embry
+// Last Updated: 9/23/2016
+//
+
+
+
 ////////// Job Role //////////
+
 // Reveal a text field when the "Other" option is selected from the "Job Role" drop down menu.
 
 $("#title").change(function() {
@@ -10,7 +19,10 @@ $("#title").change(function() {
     }
 });
 
+
+
 ////////// T-shirt Info //////////
+
 // Only display the options that match the design selected in the "Design" menu.
 
 // Function to set t-shirt dropdown menus.
@@ -41,7 +53,41 @@ $("#design").change(function() {
 // Call function to set correct t-shirt options on page load.
 setTshirtMenus();
 
+
+
 ////////// Register for Activities //////////
+
+var totalPrice = 0;
+
+// Get price of event from checkbox label text.
+var returnPrice = function (elementToCheck) {
+    var priceRegEx = /\$\d+(\.\d{1,2})?/; // RegEx to search for a price value that starts with "$".
+    var price = elementToCheck.text().match(priceRegEx); // Searches element for match with RegEx.
+    price = price[0]; // Retrieves price only (index 0) from the array.
+    price = price.replace(/[^0-9\.]+/g,""); // Removes dollar sign from front of price.
+    price = parseFloat(price); // Converts price string to float.
+    return price;
+};
+
+var addToTotal = function (price) {
+    totalPrice += price;
+    console.log(totalPrice);
+};
+
+var subtractFromTotal = function (price) {
+    totalPrice -= price;
+    console.log(totalPrice);
+};
+
+// Checkbox event listener.
+$("[type='checkbox']").change(function () {
+    var price = returnPrice($(this).parent());
+    if ($(this).prop('checked')) {
+        addToTotal(price);
+    } else {
+        subtractFromTotal(price);
+    }
+});
 
 
 
@@ -49,7 +95,11 @@ setTshirtMenus();
 
 
 
+
+
 ////////// Form Validation //////////
+
+
 
 
 
